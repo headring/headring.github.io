ecc90056438c071bc10f3001d02e6e49d758b67b
callmejeje <choije401@naver.com>
2022-09-29 21:35:20 +0900

    feat: 게시글 템플릿 생성 ((slug에 맞는 데이터 조회)

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -1,16 +1,41 @@
 import React, { FunctionComponent } from "react";
 import { graphql } from "gatsby";
-import Contents from "../components/Contents";
+import ContentsPage from "../pages/ContentsPage";
+// import { PostPageItemType } from '../types/PostItem.types'
+import { PostFrontmatterType } from "../types/PostItem.types";
+import Layout from "../components/Layout";
 
-type PostTemplateProps = {};
+type PostTemplateProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostPageItemType[]; // 존재하지 않는 타입이므로 에러가 발생하지만 일단 작성해주세요
+    };
+  };
+};
 
-const PostTemplate: FunctionComponent<PostTemplateProps> = function (props) {
-  console.log(props);
+const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
+  const {
+    node: {
+      html,
+      frontmatter: { title, summary, date, categories },
+    },
+  } = edges[0];
+  console.log("data:", title);
 
   return (
-    <div>
-      <Contents />
-    </div>
+    <Layout>
+      <ContentsPage
+        globalTheme={"light"}
+        title={title}
+        date={date}
+        categories={categories}
+        html={html}
+      />
+    </Layout>
   );
 };
 
@@ -31,3 +56,10 @@ export const queryMarkdownDataBySlug = graphql`
     }
   }
 `;
+
+export type PostPageItemType = {
+  node: {
+    html: string;
+    frontmatter: PostFrontmatterType;
+  };
+};
