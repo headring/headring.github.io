df0ad2e7d5967fcf02544104e2bec7d2613122af
callmejeje <choije401@naver.com>
2022-09-29 21:33:09 +0900

    feat: 실제 데이터 props로 넘겨줌

--- src/components/ContentsBox/index.js
+++ src/components/ContentsBox/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import Contents from "../Contents";
 // import ContentsIndex from "../ContentsIndex";
 
-function ContentsBox({ globalTheme }) {
+function ContentsBox({ globalTheme, title, date, categories, html }) {
   return (
     <>
-      <Contents globalTheme={globalTheme} />
+      <Contents
+        globalTheme={globalTheme}
+        title={title}
+        date={date}
+        categories={categories}
+        html={html}
+      />
       {/* <ContentsIndex /> */}
     </>
   );
