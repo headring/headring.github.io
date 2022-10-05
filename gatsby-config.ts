c15ce06520c1da7df53237a7379971bea6491ddc
MINDA01 <ekdud4639@gmail.com>
2022-10-06 00:30:56 +0900

    feat: gatsby-config flags 추가

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,6 +9,9 @@ const config: GatsbyConfig = {
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
+  flags: {
+    DEV_SSR: true
+  },
   graphqlTypegen: true,
   plugins: [
     //   {
